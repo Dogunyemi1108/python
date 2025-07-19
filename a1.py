33c64fd042bd43a7d25fea3560915b8d6a47391f
Dogunyemi1108 <demiog321@gmail.com>
2025-07-19 14:02:59 +0100

    python a1

---
+++ a1.py
@@ -0,0 +1,13 @@
+#Print a sentence
+print("Welcome to the world of programming")
+#Print a number
+print(4)
+
+#Next Line after print
+print("hello python \n")
+
+#Pass multiple arguments for printing
+print("Hello",5)
+
+#End argument of print statement
+print("welcome to", end="*")
