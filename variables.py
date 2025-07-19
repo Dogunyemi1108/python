b34b902c922ff06dd87c5d2c817f1fe7905c7127
Dogunyemi1108 <demiog321@gmail.com>
2025-07-19 14:10:00 +0100

    variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+#Creating different variables
+x=5
+y="john"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name=input("enter your name")
+
+print("\nHello","\nWelcomme to codingal")
