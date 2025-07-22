d78872a6fa654e02bc918614b259e3e75e768c98
Dogunyemi1108 <demiog321@gmail.com>
2025-07-22 11:30:25 +0100

    birthday.py

---
+++ birthday.py
@@ -0,0 +1,13 @@
+print("Birthday of 5 friends\n")
+
+#Storing Birthdays in variables and printing them
+a="02/08/12"
+print("Neriah -",a)
+b="12/08/12"
+print("\nSam-",b)
+c="13/05/12"
+print("\nYasin-",c)
+d="10/03/15"
+print("\nFeyi",d)
+e="11/08/12"
+print("\nDemi-",e)
