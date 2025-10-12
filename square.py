6e016711cc8a36bfab3f4e78292de581f8235de2
Dogunyemi1108 <demiog321@gmail.com>
2025-10-12 14:28:43 +0100

    square.py

---
+++ square.py
@@ -0,0 +1,17 @@
+import turtle #importing library
+
+turtle.Screen().bgcolor("Blue")
+board = turtle.Turtle()
+board.forward(120)
+board.right(90)
+
+board.forward(120)
+board.right(90)
+
+board.forward(120)
+board.right(90)
+
+board.forward(120)
+
+turtle.done()
+
