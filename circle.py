2ffdc694ce797684cc57b84d29b109284ee73df0
Dogunyemi1108 <demiog321@gmail.com>
2025-12-21 14:57:26 +0000

    circle.py

--- circle.py
+++ circle.py
@@ -9,6 +9,7 @@ window.fill((255,255,255))
 Green = (0,255,0)
 #Draw the solid circle
 pygame.draw.circle(window,Green,(100,100),50,3)
+pygame.draw.circle(window,Green,(300,300),50)
 #Draws the surface object to the screen.
 pygame.display.update()
 #Game loop
